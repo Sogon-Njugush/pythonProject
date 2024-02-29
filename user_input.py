2aeb2e68b813bf0aac4d85384da99a43b36d732d
Sogon-Njugush <sogon5045@gmail.com>
2024-02-29 23:11:22 +0300

    user inputs

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# get user's name
+name = input("What is your name? ")
+
+# get user's age
+age = input("How old are you? ")
+
+# get user's location
+location = input("Where are you located? ")
+
+# Print message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
